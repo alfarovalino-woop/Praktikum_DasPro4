b9f2b6922908a1d3f6b68cd1669d5f99ea8acf22
alfarovalino-woop <email-github-alfarovalino@gmail.com>
2026-09-22 14:01:41 +0700

    memperbarui modifikasi ternary operator

---
+++ Pemilihan2Percobaan120.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan120 {
+    public static void main(String[] args) {
+        Scanner sc20 = new Scanner(System.in);
+        System.out.print("Masukkan Tahun: ");
+        
+        int tahun = sc20.nextInt();
+        
+        System.out.println("Tahun yang dimasukkan adalah: " + tahun);
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0);
+            System.out.println("Tahun Kabisat");
+
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+    
+}
